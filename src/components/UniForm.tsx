c1864e17e1a4309cba240f3b6e7931fd759b6cde
Shun Yuan <tanshunyuan99@gmail.com>
2022-04-06 21:58:57 +0800

    feat: added field array

--- src/components/UniForm.tsx
+++ src/components/UniForm.tsx
@@ -1,61 +1,99 @@
 import { IUniversity } from '@/helpers/types';
 import { Btn } from '@/styles/common';
-import { Form, Formik } from 'formik';
+import { Form, Formik, FieldArray, Field } from 'formik';
 import { useEffect, useState } from 'react';
 import { FormInput } from './Form';
-interface IUniForm {
-  country: string;
-  domain: string;
-  webpage: string;
-}
-export const UniForm = ({
-  data = [],
-  isEdit = false,
-}: {
-  data?: IUniversity | [];
+
+type IUniForm = Pick<IUniversity, 'country' | 'domains' | 'web_pages'>;
+type UniFormProps = {
+  data?: IUniversity | undefined;
   isEdit?: boolean;
-}) => {
-  const [initialValues, setInitialValues] = useState({});
+};
+
+export const UniForm = ({ data, isEdit = false }: UniFormProps) => {
+  console.log(data);
+  const initialValues: IUniForm = {
+    country: '',
+    domains: [''],
+    web_pages: [''],
+  };
+  const [formValues, setFormValues] = useState<IUniForm>(null);
   useEffect(() => {
-    const meme: IUniForm = {
-      country: '',
-      domain: '',
-      webpage: '',
-    };
-    setInitialValues(meme);
-    if (isEdit && data !== []) {
-      setInitialValues(data);
+    if (isEdit && typeof data !== undefined) {
+      setFormValues(data);
     }
   }, [isEdit, data]);
-  const handleSubmit = () => {
+  const handleSubmit = (values) => {
     console.log('hi there');
+    console.log(values);
   };
   return (
     <Formik
       enableReinitialize={true}
-      initialValues={initialValues}
+      initialValues={formValues || initialValues}
       onSubmit={handleSubmit}
     >
       <Form>
-        {JSON.stringify(initialValues)}
         <FormInput
           name="country"
           label="Country"
           placeholder="Slovika"
           type="text"
         />
-        <FormInput
-          name="domain"
-          label="Domain"
-          placeholder="google.com.sg"
-          type="text"
-        />
-        <FormInput
-          name="webpage"
-          label="Webpage"
-          placeholder="https://google.com.sg/"
-          type="text"
-        />
+        <FieldArray name="domains">
+          {({ push, remove, form }) => {
+            const {
+              values: { domains },
+            } = form;
+            return (
+              <div>
+                <label htmlFor="domains">Domains</label>
+                {domains.map((_: string, index: number) => {
+                  return (
+                    <div key={index}>
+                      <Field
+                        name={`domains[${index}]`}
+                        placeholder="google.com"
+                        type="text"
+                      />
+                      <button onClick={() => push('')}>+</button>
+                      {index > 0 && (
+                        <button onClick={() => remove(index)}>-</button>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </FieldArray>
+        <FieldArray name="web_pages">
+          {({ push, remove, form }) => {
+            const {
+              values: { web_pages },
+            } = form;
+            return (
+              <div>
+                <label htmlFor="web_pages">Web Pages</label>
+                {web_pages.map((_: string, index: number) => {
+                  return (
+                    <div key={index}>
+                      <Field
+                        name={`web_pages[${index}]`}
+                        placeholder="https://google.com"
+                        type="text"
+                      />
+                      <button onClick={() => push('')}>+</button>
+                      {index > 0 && (
+                        <button onClick={() => remove(index)}>-</button>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </FieldArray>
         <Btn type="submit">Submit</Btn>
       </Form>
     </Formik>
