c12ff9b93d98f340e5fa6bdaa04dfeeb89451e13
shun yuan <tanshunyuan99@gmail.com>
2022-04-15 16:03:45 +0800

    chore: altered seed

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import { models, connectDb } from "./model";
-import { SeedUniversities } from "./seed/add-university";
 
 const express = require("express");
 const app = express();
@@ -9,7 +8,6 @@ const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL!;
 
 connectDb(DATABASE_URL).then(async () => {
-  //   await SeedUniversities();
   app.listen(PORT, () => {
     console.log(`server listening at ${PORT}`);
   });
--- server/seed/add-university.ts
+++ server/seed/add-university.ts
@@ -1,7 +1,14 @@
-import University from "../model/university";
-import * as UNIVERSITY_DATAS from "./data.json";
+import * as dotenv from "dotenv";
+import * as mongoose from "mongoose";
+import * as path from "path";
 
-const UniversityList = UNIVERSITY_DATAS.data.map((data) => {
+import * as UNIVERSITY_DATAS from "./data.json";
+import { connectDb } from "../model";
+import University from "../model/university";
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+const DATABASE_URL = process.env.DATABASE_URL!;
+const UniversityList = UNIVERSITY_DATAS.data.map((data: any) => {
   const {
     name,
     country,
@@ -22,8 +29,18 @@ const UniversityList = UNIVERSITY_DATAS.data.map((data) => {
   });
 });
 
-export const SeedUniversities = async () => {
-  for (let i = 0; i < UniversityList.length; i++) {
-    await UniversityList[i].save();
+const SeedUniversities = async () => {
+  for (const University of UniversityList) {
+    await University.save();
   }
 };
+const seedDb = async () => {
+  await University.deleteMany({});
+  await SeedUniversities();
+};
+
+connectDb(DATABASE_URL);
+
+seedDb().then(() => {
+  mongoose.connection.close();
+});
